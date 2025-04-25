86355c2bfbaa5a98ac55ef52ecb188ac162db716
burritoparty <117869058+burritoparty@users.noreply.github.com>
2025-04-25 13:23:16 -0700

    put placeholder pages

--- lib/screen/book_details.dart
+++ lib/screen/book_details.dart
@@ -26,148 +26,171 @@ class _BookDetailsState extends State<BookDetails> {
     for (int i = 0; i < 15; i++) {
       allTags.add("tagname$i");
     }
+    int imagesPerRow = 10;
+    int totalPages = 30;
 
     return Scaffold(
       appBar: AppBar(title: Text(widget.book.name)),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
           children: [
-            // cover image
-            const Expanded(
-              // modify flex for how much space is taken
-              flex: 1,
-              child: AspectRatio(
-                aspectRatio: 2 / 3,
-                child: Placeholder(),
-              ),
-            ),
-            // details column
-            Expanded(
-              // modify flex for how much space is taken
-              flex: 2,
-              // pad out from the image
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // name of the book
-                children: [
-                  // title
-                  Padding(
-                    // specify padding only from top and bottom
-                    padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                    child: TextField(
-                      controller: titleController,
-                      decoration: const InputDecoration(
-                        labelText: "Title",
-                        border: OutlineInputBorder(),
-                      ),
-                      onSubmitted: (newTitle) => setState(() {
-                        widget.book.name = newTitle;
-                      }),
-                    ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // cover image
+                const Expanded(
+                  // modify flex for how much space is taken
+                  flex: 1,
+                  child: AspectRatio(
+                    aspectRatio: 2 / 3,
+                    child: Placeholder(),
                   ),
-                  // author handling
-                  Padding(
-                    // specify padding only from top and bottom
-                    padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
-                    child: Autocomplete<String>(
-                      initialValue: TextEditingValue(text: widget.book.author),
-                      optionsBuilder: (TextEditingValue textEditingValue) {
-                        return allAuthors.where((a) => a
-                            .toLowerCase()
-                            .contains(textEditingValue.text.toLowerCase()));
-                      },
-                      onSelected: (sel) {
-                        setState(() {
-                          debugPrint('Selected author: $sel');
-                          widget.book.author = sel;
-                        });
-                      },
-                      fieldViewBuilder: (
-                        BuildContext context,
-                        TextEditingController textEditingController,
-                        FocusNode focusNode,
-                        VoidCallback onFieldSubmitted,
-                      ) {
-                        return TextField(
-                          controller: textEditingController,
-                          focusNode: focusNode,
+                ),
+                // details column
+                Expanded(
+                  // modify flex for how much space is taken
+                  flex: 2,
+                  // pad out from the image
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // name of the book
+                    children: [
+                      // title
+                      Padding(
+                        // specify padding only from top and bottom
+                        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                        child: TextField(
+                          controller: titleController,
                           decoration: const InputDecoration(
-                            labelText: 'Author',
+                            labelText: "Title",
                             border: OutlineInputBorder(),
                           ),
-                          onSubmitted: (value) {
-                            // tell the autocomplete to treat this as a selection
-                            onFieldSubmitted();
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // tag handling
-            // TODO: modify how tags are handled
-            Expanded(
-              // modify flex for how much space is taken
-              flex: 1,
-              child: Column(
-                crossAxisAlignment:
-                // align
-                    CrossAxisAlignment.start,
-                children: [
-                  Autocomplete<String>(
-                    optionsBuilder: (TextEditingValue textEditingValue) {
-                      return allTags.where((a) => a
-                          .toLowerCase()
-                          .contains(textEditingValue.text.toLowerCase()));
-                    },
-                    // when selected
-                    onSelected: (sel) {
-                      setState(() {
-                        debugPrint('Selected tag: $sel');
-                        widget.book.tags.add(sel);
-                      });
-                    },
-                    fieldViewBuilder: (
-                      BuildContext context,
-                      TextEditingController textEditingController,
-                      FocusNode focusNode,
-                      VoidCallback onFieldSubmitted,
-                    ) {
-                      return TextField(
-                        controller: textEditingController,
-                        focusNode: focusNode,
-                        decoration: const InputDecoration(
-                          labelText: 'Add tag',
-                          border: OutlineInputBorder(),
-                        ),
-                        onSubmitted: (value) => setState(() {
-                          onFieldSubmitted();
-                        }),
-                      );
-                    },
-                  ),
-                  Padding(
-                    // padding above the tag
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Wrap(
-                      // Use Wrap instead of ListView
-                      spacing: 8.0, // space between chips
-                      runSpacing: 4.0, // space between lines
-                      children: widget.book.tags.map((tag) {
-                        return InputChip(
-                          label: Text(tag),
-                          onDeleted: () => setState(() {
-                            widget.book.tags.remove(tag);
+                          onSubmitted: (newTitle) => setState(() {
+                            widget.book.name = newTitle;
                           }),
-                        );
-                      }).toList(),
-                    ),
+                        ),
+                      ),
+                      // author handling
+                      Padding(
+                        // specify padding only from top and bottom
+                        padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                        child: Autocomplete<String>(
+                          initialValue:
+                              TextEditingValue(text: widget.book.author),
+                          optionsBuilder: (TextEditingValue textEditingValue) {
+                            return allAuthors.where((a) => a
+                                .toLowerCase()
+                                .contains(textEditingValue.text.toLowerCase()));
+                          },
+                          onSelected: (sel) {
+                            setState(() {
+                              debugPrint('Selected author: $sel');
+                              widget.book.author = sel;
+                            });
+                          },
+                          fieldViewBuilder: (
+                            BuildContext context,
+                            TextEditingController textEditingController,
+                            FocusNode focusNode,
+                            VoidCallback onFieldSubmitted,
+                          ) {
+                            return TextField(
+                              controller: textEditingController,
+                              focusNode: focusNode,
+                              decoration: const InputDecoration(
+                                labelText: 'Author',
+                                border: OutlineInputBorder(),
+                              ),
+                              onSubmitted: (value) {
+                                // tell the autocomplete to treat this as a selection
+                                onFieldSubmitted();
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
+                // tag handling
+                // TODO: modify how tags are handled
+                Expanded(
+                  // modify flex for how much space is taken
+                  flex: 2,
+                  child: Column(
+                    crossAxisAlignment:
+                        // align
+                        CrossAxisAlignment.start,
+                    children: [
+                      Autocomplete<String>(
+                        optionsBuilder: (TextEditingValue textEditingValue) {
+                          return allTags.where((a) => a
+                              .toLowerCase()
+                              .contains(textEditingValue.text.toLowerCase()));
+                        },
+                        // when selected
+                        onSelected: (sel) {
+                          setState(() {
+                            debugPrint('Selected tag: $sel');
+                            widget.book.tags.add(sel);
+                          });
+                        },
+                        fieldViewBuilder: (
+                          BuildContext context,
+                          TextEditingController textEditingController,
+                          FocusNode focusNode,
+                          VoidCallback onFieldSubmitted,
+                        ) {
+                          return TextField(
+                            controller: textEditingController,
+                            focusNode: focusNode,
+                            decoration: const InputDecoration(
+                              labelText: 'Add tag',
+                              border: OutlineInputBorder(),
+                            ),
+                            onSubmitted: (value) => setState(() {
+                              onFieldSubmitted();
+                            }),
+                          );
+                        },
+                      ),
+                      Padding(
+                        // padding above the tag
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Wrap(
+                          // Use Wrap instead of ListView
+                          spacing: 8.0, // space between chips
+                          runSpacing: 4.0, // space between lines
+                          children: widget.book.tags.map((tag) {
+                            return InputChip(
+                              label: Text(tag),
+                              onDeleted: () => setState(() {
+                                widget.book.tags.remove(tag);
+                              }),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.only(top: 16.0),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: imagesPerRow,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 12,
+                  childAspectRatio: 2 / 3, 
+                ),
+                itemCount: totalPages,
+                itemBuilder: (context, index) {
+                  // TODO: replace with Image.File
+                  return const Placeholder(); 
+                },
               ),
             ),
           ],
