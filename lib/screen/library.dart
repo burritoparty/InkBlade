ce131daeb09b5b346b39e6d8fd1bce433f6100a2
burritoparty <117869058+burritoparty@users.noreply.github.com>
2025-04-25 21:03:37 -0700

    organized library with classes

--- lib/screen/library.dart
+++ lib/screen/library.dart
@@ -1,107 +1,135 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_manga_reader/book.dart';
 import 'book_details.dart';
 
+// main library screen, holds state for the list of books
 class Library extends StatefulWidget {
   const Library({super.key});
 
   @override
-  State<Library> createState() => _LibraryState();
+  LibraryState createState() => LibraryState();
 }
 
-class _LibraryState extends State<Library> {
-  // setting up dummy
-  // TODO: load from json
+class LibraryState extends State<Library> {
+  // temporary in‐memory list until we load from JSON
   late List<Book> temporaryBooks;
+
   @override
   void initState() {
     super.initState();
+    // generate dummy books for now
     temporaryBooks = List.generate(
-        20, (i) =>
-         Book("name$i", "author", ["tag1", "tag2", "tag3"], "link",
-            "C:\\path", "coverPath", false, false));
+      20,
+      (i) => Book(
+        "name$i", 
+        "author", 
+        ["tag1", "tag2", "tag3"], 
+        "link",
+        "C:\\path", 
+        "coverPath", 
+        false, // favorite?
+        false, // readLater?
+      ),
+    );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    // delegate grid layout + tap handling to BookGrid
+    return BookGrid(
+      books: temporaryBooks,
+      onBookTap: (index) async {
+        // open details, then refresh when returned
+        await Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => BookDetails(book: temporaryBooks[index]),
+          ),
+        );
+        setState(() { /* rebuild to pick up any changes */ });
+      },
+    );
+  }
+}
+
+// single book tile: title, image, ripple efx
+class BookTile extends StatelessWidget {
+  final Book book;
+  final VoidCallback onTap;
+
+  const BookTile({
+    Key? key,
+    required this.book,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      // rounded corners if you want
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onTap,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // book title at top, ellipsize if too long
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 4.0, horizontal: 8.0),
+                child: Text(
+                  book.title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              // placeholder for cover
+              const Expanded(child: Placeholder()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// responsive grid of BookTiles
+class BookGrid extends StatelessWidget {
+  final List<Book> books;
+  final ValueChanged<int> onBookTap;
+
+  const BookGrid({
+    Key? key,
+    required this.books,
+    required this.onBookTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        // setting button size
-        final double maxWidth = constraints.maxWidth;
-        int columns = (maxWidth / 300)
-            .floor(); // modify the maxWidth / number to change size
-        if (columns < 1) columns = 1;
+        // calculate how many columns fit at ~300px each
+        int columns = (constraints.maxWidth / 300).floor().clamp(1, books.length);
 
-        // make the gridview
         return GridView.builder(
           padding: const EdgeInsets.all(8),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: columns,
             crossAxisSpacing: 8,
             mainAxisSpacing: 8,
-            childAspectRatio: 0.667, // adjust for button shape
+            childAspectRatio: 2/3, // tweak for tile shape
           ),
-          itemCount: temporaryBooks.length,
-          // building the buttons
-          itemBuilder: (context, index) {
-            return ClipRRect(
-              // borderRadius: BorderRadius.circular(12),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  // pulls up the book details
-                  onTap: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) =>
-                            BookDetails(book: temporaryBooks[index]),
-                      ),
-                    );
-                    setState( // updating library state after returning from BookDetails
-                      () {},
-                    ); 
-                  },
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      // book title at the top
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4.0, horizontal: 8.0),
-                        child: Text(
-                          temporaryBooks[index].title,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-
-                      // cover, filling remaining space
-                      const Expanded(
-                        child: Placeholder(),
-                      )
-                      // uncomment this to actually get the image
-                      // Expanded(
-                      //   child: Image.file(
-                      //     File(books[index].coverPath),
-                      //     width: double.infinity,
-                      //     // height is defined by Expanded
-                      //     fit: BoxFit.contain,
-                      //     alignment: Alignment.bottomCenter,
-                      //   ),
-                      // ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
+          itemCount: books.length,
+          itemBuilder: (context, i) => BookTile(
+            book: books[i],
+            onTap: () => onBookTap(i),
+          ),
         );
       },
     );
