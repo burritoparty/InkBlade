737e63cc5a1e9e9f2f105102fe8fcd65e4cb2261
burritoparty <117869058+burritoparty@users.noreply.github.com>
2025-05-01 16:31:40 -0700

    added colors to author text entry

--- lib/widgets/author_editor.dart
+++ lib/widgets/author_editor.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AuthorEditor extends StatelessWidget {
+class AuthorEditor extends StatefulWidget {
   final String initialAuthor;
   final List<String> allAuthors;
   final ValueChanged<String> onSelected;
@@ -13,20 +13,42 @@ class AuthorEditor extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _AuthorEditorState createState() => _AuthorEditorState();
+}
+
+class _AuthorEditorState extends State<AuthorEditor> {
+  // tracks unsaved edits
+  bool _isDirty = false;
+  // tracks whether we've ever saved
+  bool _hasSaved = false;
+
+  @override
   Widget build(BuildContext context) {
+    // pick border/label color: gray on pristine, red when dirty, green after save
+    final Color activeColor = _isDirty
+        ? Colors.red
+        : (_hasSaved ? Colors.green : Colors.grey);
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Autocomplete<String>(
-        initialValue: TextEditingValue(text: initialAuthor),
+        initialValue: TextEditingValue(text: widget.initialAuthor),
         optionsBuilder: (TextEditingValue textEditingValue) {
-          // don't show options until something is typed
           if (textEditingValue.text.isEmpty) {
             return const Iterable<String>.empty();
           }
           final input = textEditingValue.text.toLowerCase();
-          return allAuthors.where((a) => a.toLowerCase().contains(input));
+          return widget.allAuthors
+              .where((a) => a.toLowerCase().contains(input));
         },
-        onSelected: onSelected,
+        onSelected: (value) {
+          // mark saved on dropdown select
+          setState(() {
+            _isDirty = false;
+            _hasSaved = true;
+          });
+          widget.onSelected(value);
+        },
         fieldViewBuilder: (
           BuildContext context,
           TextEditingController textEditingController,
@@ -36,11 +58,34 @@ class AuthorEditor extends StatelessWidget {
           return TextField(
             controller: textEditingController,
             focusNode: focusNode,
-            decoration: const InputDecoration(
+            decoration: InputDecoration(
               labelText: 'Author',
-              border: OutlineInputBorder(),
+              // label color matches border
+              labelStyle: TextStyle(color: activeColor),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: activeColor),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: activeColor),
+              ),
             ),
-            onSubmitted: (_) => onFieldSubmitted(),
+            onChanged: (_) {
+              // turn dirty on any change
+              if (!_isDirty) {
+                setState(() {
+                  _isDirty = true;
+                });
+              }
+            },
+            onSubmitted: (_) {
+              // save on enter
+              onFieldSubmitted();
+              setState(() {
+                _isDirty = false;
+                _hasSaved = true;
+              });
+              widget.onSelected(textEditingController.text);
+            },
           );
         },
       ),
