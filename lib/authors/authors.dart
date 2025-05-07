358b18e58790cb6a35a630a9453f3f5f71689514
burritoparty <117869058+burritoparty@users.noreply.github.com>
2025-05-06 19:01:28 -0700

    implemented library controller for author page

--- lib/authors/authors.dart
+++ lib/authors/authors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_manga_reader/router/routes.dart';
-import '../models/book.dart';
+import 'package:provider/provider.dart';
+import '../controllers/library_controller.dart'; 
 
 class Authors extends StatefulWidget {
   const Authors({super.key});
@@ -11,87 +12,21 @@ class Authors extends StatefulWidget {
 
 class _AuthorsState extends State<Authors> {
   final TextEditingController _searchController = TextEditingController();
-  List<Book> allBooks = [];
-  List<String> allAuthors = [];
   List<String> filteredAuthors = [];
+
   @override
   void initState() {
     super.initState();
-
-    // load up the authors
-    final allBooks = [
-      Book(
-        path: "C:\\",
-        title: "Full Metal Alchemist Brotherhood",
-        link: "link",
-        series: "Full Metal Alchemist",
-        authors: ["Hiromu Arakawa"],
-        tags: ["Adventure", "Fantasy"],
-        characters: ["Edward", "Alphonse", "Winry"],
-        favorite: true,
-        readLater: false,
-      ),
-      Book(
-        path: "C:\\",
-        title: "My Dress Up Darling: Volume 1",
-        link: "link",
-        series: "My Dress Up Darling",
-        authors: ["Shinichi Fukuda"],
-        tags: ["Romance", "Comedy", "Cosplay"],
-        characters: ["Marin Kitagawa", "Gojo"],
-        favorite: true,
-        readLater: false,
-      ),
-      Book(
-        path: "C:\\",
-        title: "My Dress Up Darling: Volume 2",
-        link: "link",
-        series: "My Dress Up Darling",
-        authors: ["Shinichi Fukuda"],
-        tags: ["Romance", "Comedy", "Cosplay"],
-        characters: ["Marin Kitagawa", "Wakana Gojo"],
-        favorite: true,
-        readLater: false,
-      ),
-      Book(
-        path: "C:\\",
-        title: "Komi Can't Communicate: Volume 1",
-        link: "link",
-        series: "Komi Can't Communicate",
-        authors: ["Tomohito Oda"],
-        tags: ["Romance", "Comedy", "Slice of Life"],
-        characters: ["Komi Shoko", "Tadano Hitohito"],
-        favorite: false,
-        readLater: true,
-      ),
-      Book(
-        path: "C:\\",
-        title: "Hokkaido Gals Are Super Adorable: Volume 1",
-        link: "link",
-        series: "Hokkaido Gals Are Super Adorable",
-        authors: ["Ikada Kai"],
-        tags: ["Romance", "Comedy"],
-        characters: ["Fuyuki Minami", "Akino Sayuri", "Shiki Tsubasa"],
-        favorite: false,
-        readLater: true,
-      ),
-    ];
-
-    // look through the books and only load up the ones with author
-    for (Book book in allBooks) {
-      // iterate thru every books authors
-      for (String author in book.authors) {
-        // if new author add to list
-        if (!allAuthors.contains(author)) {
-          allAuthors.add(author);
-        }
-      }
-    }
-
-    filteredAuthors = allAuthors;
+    // Initialize filteredAuthors with all authors from the LibraryController
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final libraryController = context.read<LibraryController>();
+      setState(() {
+        filteredAuthors = libraryController.authors.toList();
+      });
+    });
   }
 
-  void filterAuthors(String query) {
+  void filterAuthors(String query, List<String> allAuthors) {
     setState(() {
       if (query.isEmpty) {
         filteredAuthors = List.from(allAuthors);
@@ -106,6 +41,12 @@ class _AuthorsState extends State<Authors> {
 
   @override
   Widget build(BuildContext context) {
+    // set up the library controller, which holds the list of books
+    // it watches for changes to the list of books, and rebuilds the widget tree
+    final libraryController = context.watch<LibraryController>();
+    // Filter the authors dynamically
+    final allAuthors = libraryController.authors.toList();
+
     return Column(
       children: [
         Center(
@@ -115,7 +56,7 @@ class _AuthorsState extends State<Authors> {
               controller: _searchController,
               hintText: 'Search authors...',
               onChanged: (value) {
-                filterAuthors(value);
+                filterAuthors(value, allAuthors);
               },
             ),
           ),
@@ -140,7 +81,7 @@ class AuthorButtons extends StatelessWidget {
     return Expanded(
       child: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          double buttonWidth = 150.0;
+          double buttonWidth = 175.0;
           int crossAxisCount = (constraints.maxWidth / buttonWidth).floor();
           if (crossAxisCount < 1) {
             crossAxisCount = 1;
