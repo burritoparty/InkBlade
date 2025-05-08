f271995fefb05d40920cd041aee25a1a5875aece
burritoparty <117869058+burritoparty@users.noreply.github.com>
2025-05-07 21:46:42 -0700

    book reader now hides curser

--- lib/screen/book/book_reader.dart
+++ lib/screen/book/book_reader.dart
@@ -27,6 +27,10 @@ class BookReaderState extends State<BookReader> {
   Timer? _scrollTimer;
   Timer? _pageTurnDebounce;
 
+  // cursor visibility
+  bool _cursorVisible = true;
+  Timer? _cursorTimer;
+
   @override
   void initState() {
     super.initState();
@@ -39,6 +43,7 @@ class BookReaderState extends State<BookReader> {
   void dispose() {
     _scrollTimer?.cancel();
     _pageTurnDebounce?.cancel();
+    _cursorTimer?.cancel();
     _pageController.dispose();
     _scrollController.dispose();
     super.dispose();
@@ -191,117 +196,135 @@ class BookReaderState extends State<BookReader> {
     }
   }
 
+  // method to reset the cursor visibility timer
+  void _resetCursorTimer() {
+    _cursorTimer?.cancel();
+    setState(() {
+      _cursorVisible = true;
+    });
+    _cursorTimer = Timer(const Duration(seconds: 1), () {
+      setState(() {
+        _cursorVisible = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // get the list of pages from the book
     final pages = widget.book.getPageFiles();
     // get the total number of pages
     final totalPages = pages.length;
-    return KeyboardListener(
-      // makes sure this recieves the key events
-      focusNode: FocusNode()..requestFocus(),
-      onKeyEvent: _handleKey,
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.home),
-              onPressed: () {
-                // navigate back to the home screen
-                Navigator.popUntil(
-                  context,
-                  ModalRoute.withName('/'),
-                );
-              },
-            ),
-          ],
-          leading: IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () => Navigator.pop(context)),
-          // show page indicator, opens dialog on tap
-          title: GestureDetector(
-              onTap: _showJumpToPageDialog,
-              child: Text('${_currentPage + 1} of $totalPages')),
-          centerTitle: true,
-        ),
-        body: PageView.builder(
-          controller: _pageController,
-          itemCount: totalPages,
-          // reset scroll and update page
-          onPageChanged: (i) {
-            _resetScroll();
-            setState(() {
-              _currentPage = i;
-            });
-          },
-          itemBuilder: (context, index) {
-            // get the file for this page
-            final file = pages[index];
-            if (_zoomedIn) {
-              // scale zoomed in view to x of screen width
-              final screenW = MediaQuery.of(context).size.width;
-              return SingleChildScrollView(
-                controller: _scrollController,
-                child: Center(
-                  child: Image.file(
-                    file,
-                    width: screenW * 0.6, // change screen width here
-                    fit: BoxFit.fitWidth,
-                    errorBuilder: (_, __, ___) =>
-                        const Center(child: Text('Image not found')),
-                  ),
-                ),
-              );
-            } else {
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: AspectRatio(
-                  aspectRatio: 2 / 3,
-                  child: Image.file(
-                    file,
-                    fit: BoxFit.contain,
-                    errorBuilder: (_, __, ___) =>
-                        const Center(child: Text('Image not found')),
-                  ),
-                ),
-              );
-            }
-          },
-        ),
-        bottomNavigationBar: Container(
-          color: Colors.blueGrey[900],
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          height: 56,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // button to jump to first page
+    return MouseRegion(
+      onHover: (_) => _resetCursorTimer(),
+      cursor:
+          _cursorVisible ? SystemMouseCursors.basic : SystemMouseCursors.none,
+      child: KeyboardListener(
+        // makes sure this receives the key events
+        focusNode: FocusNode()..requestFocus(),
+        onKeyEvent: _handleKey,
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
               IconButton(
-                  icon: const Icon(Icons.first_page, color: Colors.white),
-                  onPressed: () => _goToPage(0)),
-              // button to jump to previous page
-              IconButton(
-                  icon: const Icon(Icons.chevron_left, color: Colors.white),
-                  onPressed: () => _goToPage(_currentPage - 1)),
-              // zoom toggle
-              IconButton(
-                icon: Icon(_zoomedIn ? Icons.zoom_out : Icons.zoom_in,
-                    color: Colors.white),
+                icon: const Icon(Icons.home),
                 onPressed: () {
-                  _zoomedIn = !_zoomedIn;
-                  _resetScroll();
-                  setState(() {});
+                  // navigate back to the home screen
+                  Navigator.popUntil(
+                    context,
+                    ModalRoute.withName('/'),
+                  );
                 },
               ),
-              // button next page
-              IconButton(
-                  icon: const Icon(Icons.chevron_right, color: Colors.white),
-                  onPressed: () => _goToPage(_currentPage + 1)),
-              // button previous page
-              IconButton(
-                  icon: const Icon(Icons.last_page, color: Colors.white),
-                  onPressed: () => _goToPage(totalPages - 1)),
             ],
+            leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context)),
+            // show page indicator, opens dialog on tap
+            title: GestureDetector(
+                onTap: _showJumpToPageDialog,
+                child: Text('${_currentPage + 1} of $totalPages')),
+            centerTitle: true,
+          ),
+          body: PageView.builder(
+            controller: _pageController,
+            itemCount: totalPages,
+            // reset scroll and update page
+            onPageChanged: (i) {
+              _resetScroll();
+              setState(() {
+                _currentPage = i;
+              });
+            },
+            itemBuilder: (context, index) {
+              // get the file for this page
+              final file = pages[index];
+              if (_zoomedIn) {
+                // scale zoomed in view to x of screen width
+                final screenW = MediaQuery.of(context).size.width;
+                return SingleChildScrollView(
+                  controller: _scrollController,
+                  child: Center(
+                    child: Image.file(
+                      file,
+                      width: screenW * 0.6, // change screen width here
+                      fit: BoxFit.fitWidth,
+                      errorBuilder: (_, __, ___) =>
+                          const Center(child: Text('Image not found')),
+                    ),
+                  ),
+                );
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: AspectRatio(
+                    aspectRatio: 2 / 3,
+                    child: Image.file(
+                      file,
+                      fit: BoxFit.contain,
+                      errorBuilder: (_, __, ___) =>
+                          const Center(child: Text('Image not found')),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+          bottomNavigationBar: Container(
+            color: Colors.blueGrey[900],
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            height: 56,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // button to jump to first page
+                IconButton(
+                    icon: const Icon(Icons.first_page, color: Colors.white),
+                    onPressed: () => _goToPage(0)),
+                // button to jump to previous page
+                IconButton(
+                    icon: const Icon(Icons.chevron_left, color: Colors.white),
+                    onPressed: () => _goToPage(_currentPage - 1)),
+                // zoom toggle
+                IconButton(
+                  icon: Icon(_zoomedIn ? Icons.zoom_out : Icons.zoom_in,
+                      color: Colors.white),
+                  onPressed: () {
+                    _zoomedIn = !_zoomedIn;
+                    _resetScroll();
+                    setState(() {});
+                  },
+                ),
+                // button next page
+                IconButton(
+                    icon: const Icon(Icons.chevron_right, color: Colors.white),
+                    onPressed: () => _goToPage(_currentPage + 1)),
+                // button previous page
+                IconButton(
+                    icon: const Icon(Icons.last_page, color: Colors.white),
+                    onPressed: () => _goToPage(totalPages - 1)),
+              ],
+            ),
           ),
         ),
       ),
